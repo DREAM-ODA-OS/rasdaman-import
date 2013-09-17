e01c4a696cb8362dfd9b458bb8552ffe1b6deaf4
Dimitar Misev <misev@rasdaman.com>
2013-09-17 10:41:40 +0200

    USGS DEM import scripts

---
+++ import/USGS_DEM/drop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+# Description   Script to drop data from rasdaman and petascope
+# Dependencies  rasdaman, postgres
+#
+# Date          2013-mar-16
+# Author        Dimitar Misev
+# ----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
+# script initialization
+# ----------------------------------------------------------------------------
+
+# script name
+PROG=`basename $0`
+
+# determine script directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+IMPORT_SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# include import data configuration and utility functions
+. $IMPORT_SCRIPT_DIR/import.cfg
+
+# ----------------------------------------------------------------------------
+# work
+# ----------------------------------------------------------------------------
+
+drop_colls
+drop_petascope
+
+log "done."
---
+++ import/USGS_DEM/import.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+# Description   Script to import data to rasdaman and petascope
+# Dependencies  rasdaman, postgres
+#
+# Date          2013-mar-16
+# Author        Dimitar Misev
+# ----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
+# script initialization
+# ----------------------------------------------------------------------------
+
+# script name
+PROG=`basename $0`
+
+# determine script directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+IMPORT_SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# include import data configuration and utility functions
+. $IMPORT_SCRIPT_DIR/import.cfg
+
+# ----------------------------------------------------------------------------
+# import initialization
+# ----------------------------------------------------------------------------
+
+# check dependencies
+check_rasdaman
+
+# update coll/cov lists
+update_covs
+update_colls
+
+# read rasdl types from types.dl file
+read_types
+
+function initcolls()
+{
+for c in $COLLS; do
+  logn "initializing collection $c... "
+  $RASQL -q "create collection $c $SET_TYPE" > /dev/null
+  feedback
+done
+}
+
+# ----------------------------------------------------------------------------
+# import data to rasdaman
+# ----------------------------------------------------------------------------
+
+function importras()
+{
+pushd $DATADIR > /dev/null
+
+for c in $COLLS; do
+  logn "importing $c... "
+  $RASQL -q "insert into $c values (short) inv_tiff(\$1)" -f $c.tif > /dev/null
+  feedback
+done
+
+popd > /dev/null
+}
+
+# ----------------------------------------------------------------------------
+# import data to petascope
+# ----------------------------------------------------------------------------
+
+function importpet()
+{
+for c in $COVS; do
+  import_petascope "$c" "$axes_names" "$CRS"
+done
+}
+
+# ----------------------------------------------------------------------------
+# actual work
+# ----------------------------------------------------------------------------
+
+initcolls
+importras
+importpet
+
+log "done."
